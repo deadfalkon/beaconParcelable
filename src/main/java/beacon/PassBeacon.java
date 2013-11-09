2b9eb9447c5a0d3bc65fdaede11c981a35985c92
Falko Richter <falko@briefhansa.de>
2013-11-09 03:35:09 +0100

    gradle file and PassBeacon Java class ftw

---
+++ src/main/java/beacon/PassBeacon.java
@@ -0,0 +1,47 @@
+package beacon;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.UUID;
+
+public class PassBeacon implements Parcelable {
+    public UUID proximityUUID;
+    public int major;
+    public int minor;
+    public String relevantText;
+
+    public PassBeacon() {
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.proximityUUID.toString());
+        dest.writeInt(this.major);
+        dest.writeInt(this.minor);
+        dest.writeString(this.relevantText);
+    }
+
+    public PassBeacon(Parcel in) {
+        this.proximityUUID = UUID.fromString(in.readString());
+        this.major = in.readInt();
+        this.minor = in.readInt();
+        this.relevantText = in.readString();
+    }
+
+    public static Creator<PassBeacon> CREATOR = new Creator<PassBeacon>() {
+        public PassBeacon createFromParcel(Parcel source) {
+            return new PassBeacon(source);
+        }
+
+        public PassBeacon[] newArray(int size) {
+            return new PassBeacon[size];
+        }
+    };
+}
